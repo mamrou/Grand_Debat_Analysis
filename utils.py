78b07e5a184842462ae322d9f29a55c4c14d56f8
Matyas <matyas.amrouche@telecom-paristech.fr>
2019-06-19 23:36:08 +0200

    new wordcloud type (PIL image display) implemented

--- utils.py
+++ utils.py
@@ -5,9 +5,15 @@ import random
 import numpy as np
 from wordcloud import WordCloud
 
-# test commmit
+import matplotlib
+
+# Special backend needed on macOS
+matplotlib.use('TkAgg')
+#import matplotlib.pyplot as plt
 
 
+
+# First type of wordcloud
 def plotly_wordcloud(top_words):
     words = []
     frequency = []
@@ -43,26 +49,48 @@ def plotly_wordcloud(top_words):
 
 
 
+#  Second type of wordcloud
+def word_cloud_image(top_words):
+    word_freq_dict = {}
+    for word_freq in top_words:
+        word_freq_dict[word_freq[0]] = word_freq[1]
 
-    '''def word_cloud_image(top_words):
-        word_freq_dict = {}
-        for word_freq in top_words:
-            word_freq_dict[word_freq[0]] = word_freq[1]
+    wordcloud = WordCloud(max_font_size=40, background_color="white")
+    wordcloud.generate_from_frequencies(word_freq_dict)
+    word_cloud_image = wordcloud.to_image()
 
-        wordcloud = WordCloud(max_font_size=40, background_color="white")
-        wordcloud.generate_from_frequencies(word_freq_dict)
-        word_cloud_image = wordcloud.to_image()
+    img_width = 2240
+    img_height = 1000
+    scale_factor = 0.5
 
-        data = []
+    layout = go.Layout(
+        xaxis = go.layout.XAxis(
+            visible = False,
+            range = [0, img_width*scale_factor]),
+        yaxis = go.layout.YAxis(
+            visible=False,
+            range = [0, img_height*scale_factor],
+            # the scaleanchor attribute ensures that the aspect ratio stays constant
+            scaleanchor = 'x'),
+        width = img_width*scale_factor,
+        height = img_height*scale_factor,
+        margin = {'l': 0, 'r': 0, 't': 0, 'b': 0},
+        images = [go.layout.Image(
+            x=0,
+            sizex=img_width*scale_factor,
+            y=img_height*scale_factor,
+            sizey=img_height*scale_factor,
+            xref="x",
+            yref="y",
+            opacity=1.0,
+            layer="below",
+            sizing="stretch",
+            source=word_cloud_image)]
+    )
 
-        layout = go.Layout(images=[dict(
-              source=word_cloud_image,
-              xref="paper", yref="paper",
-              x=1, y=1.05,
-              sizex=0.2, sizey=0.2,
-              xanchor="right", yanchor="bottom"
-            )],
-              {'xaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
-               'yaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False}})
+    data=[{'x': [0, img_width*scale_factor],
+            'y': [0, img_height*scale_factor],
+            'mode': 'markers',
+            'marker': {'opacity': 0}}]
 
-        return data, layout'''
+    return data, layout

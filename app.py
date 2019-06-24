88ed5a549472e04b73c0e0421befaa812c741e27
Mathieu Bruniquel <mathieu.bruniquel@gmail.com>
2019-06-24 19:26:10 +0200

    Look and feel new updates

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ import plotly.plotly as py
 import json
 import plotly.tools as tools
 from utils import *
+import seaborn as sns
+import numpy as np
 
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 #external_stylesheets = ["https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"]
 
+mapbox_access_token = 'pk.eyJ1IjoibWFyaWVpIiwiYSI6ImNqeDR2bG1ybjAxcmc0OG4wdmNza2luYXkifQ.hECaeMskkpv2CoirEUywlg'
 
 
 
@@ -22,10 +25,13 @@ data_eco = json.loads(open('data/ecologie.json').read())
 data_org = json.loads(open('data/organisation.json').read())
 text_dict = json.loads(open('data/theme.txt').read())
 
-themes = {'Dem':"Démocratie et Citoyenneté", 'Fis':"Fiscalité", 'Eco':"Ecologie", 'Org':"Organisation"}
+with open("data/departements-version-simplifiee.geojson") as geofile:
+    geojson_layer = json.load(geofile)
+
+
+themes = {'Dem':"Démocratie", 'Fis':"Fiscalité", 'Eco':"Ecologie", 'Org':"Organisation"}
 data_dict = {'Dem':data_dem, 'Fis':data_fisc, 'Eco':data_eco, 'Org':data_org}
 
-overall_stats = ['n_questions', 'n_answers','n_participants', 'answer_rate', 'avg_answer_per_participant']
 n_answers = {'Dem':2768140, 'Fis':1101287, 'Eco':1857257, 'Org':1414633}
 
 def get_data(data_key):
@@ -35,19 +41,22 @@ def get_num_answer(data_key):
     return n_answers[data_key]
 
 
-def get_stats(data_dict, overall_stats):
-    n_questions=[]
+def get_stats(data_dict, n_answers):
     n_participants = []
-    answer_rate = []
-    avg_answer_per_participant = []
+    completion_rates = []
+    n_questions = []
 
     for i,data in enumerate(data_dict.values()):
-        n_questions.append(data['n_questions'])
-        n_participants.append(data['n_participants'])
-        answer_rate.append(data['answer_rate'])
-        avg_answer_per_participant.append(data['avg_answer_per_participant'])
+        n_question = data['n_questions']
+        n_answer = list(n_answers.values())[i]
+        n_participant = data['n_participants']
+        completion_rate = n_answer/(n_participant*n_question)
 
-    return n_questions, n_participants, answer_rate, avg_answer_per_participant
+        n_questions.append(n_question)
+        n_participants.append(n_participant)
+        completion_rates.append(completion_rate)
+
+    return n_participants, completion_rates, n_questions
 
 
 def get_questions(data):
@@ -95,14 +104,152 @@ def color_question(questions_num, selected_question, answer_rate):
 
         # Annotation list
         annotations.append(dict(x=question, y=answer_rate[i], text="{0:.0f}".format(answer_rate[i]*100) + "%",
-                            font=dict(family='Arial', size=14,
+                            font=dict(family='Arial', size=26,
                             color=bar_colors[i]),
                             showarrow=False,
                             yshift = 16))
 
     return bar_colors, annotations
 
-def theme_opacity(themes, selected_theme, n_questions, n_answers, n_participants, avg_answer_per_participant):
+
+def centeroidnp(arr):
+    arr = np.array(arr)
+    length = arr.shape[0]
+    sum_x = np.sum(arr[:, 0])
+    sum_y = np.sum(arr[:, 1])
+    return [sum_x/length, sum_y/length]
+
+
+# Define color for map per department
+# color = sns.diverging_palette(10, 220, sep=10, n=100)
+color = sns.diverging_palette(10, 220, sep=20, n=24)
+color = color.as_hex()
+
+
+# Store macro-data at departement level (centroid, code, name) for hovering
+departement = [
+    dict(
+        code = geojson_layer['features'][k]['properties']['code'],
+        name=geojson_layer['features'][k]['properties']['nom'],
+        centroid=centeroidnp(geojson_layer['features'][k]['geometry']['coordinates'][0])
+    )
+    for k in range(len(geojson_layer['features']))]
+
+
+def process_data(dic):
+
+    for k, v in dic.items():
+        if k.startswith('q'):
+            if v['type']=='open':
+                continue
+            # For each question, get the list of pct_yes_per_dep
+#             print(k)
+            else:
+                sub_dic = dic[k]['pct_yes_per_dep']
+    #             print(sub_dic)
+                # Replace the dpt 97/98 by 2A/2B
+                sub_dic['2A'] = sub_dic.pop('97')
+                sub_dic['2B'] = sub_dic.pop('98')
+
+                # Update dict by reviewed department names
+                dic[k]['pct_yes_per_dep'] = sub_dic
+                # print(dic[k]['pct_yes_per_dep'])
+                for key, value in dic[k]['pct_yes_per_dep'].items():
+                    # print(key)
+                    # print(value)
+                    if key == 'all':
+                        continue
+                    else:
+
+                        dic[k]['pct_yes_per_dep'][key] = [value, value - dic[k]['pct_yes_per_dep'].get('all', 0)]
+
+    return dic
+
+# Define lon and lat of department centroid
+lon = [dep['centroid'][0] for dep in departement]
+lat = [dep['centroid'][1] for dep in departement]
+
+
+# Modify department keys
+data_dem = process_data(data_dem)
+data_fisc = process_data(data_fisc)
+data_eco = process_data(data_eco)
+data_org = process_data(data_org)
+
+def define_index_for_color(delta):
+    '''
+    Given a delta vs. average, define the index of color to display at departement level
+    '''
+    # Scale to percentage
+    delta = round(delta*100 + 11.5)
+
+    # Index of colours goes from 0 to 23, we cap the potential index
+    adjusted_value = min(23, max(0, delta))
+
+    return adjusted_value
+
+def get_closed_question_params(data, selected_question):
+
+    dic_answers = data[selected_question.lower()]['pct_yes_per_dep']
+
+    # Define text that will appear when hovering over a department
+    hovering_text = ['{} - {}<br>Pourcentage de Oui : {}%<br>Ecart vs. moy. : {}%'.format(dep['code'],dep['name'], round(100*dic_answers[dep['code']][0]), round(100*dic_answers[dep['code']][1])) for dep in departement]
+
+    data = [
+        go.Scattermapbox(
+            lat= lat,
+            lon=lon,
+            mode='markers',
+            text=hovering_text,
+            marker=dict(
+                size=0.5,
+                color= '#a490bd',
+                colorbar=dict(
+                    title = '% Oui vs. moyenne',
+                    titleside = 'top',
+                    tickmode = 'array',
+                    tickvals = [0,4.5,9],
+                    ticktext = ['>-10','0','>+10'],
+                    ticks = 'outside'
+                ),
+            colorscale=[[idx/23, elem] for idx, elem in enumerate(sns.diverging_palette(10, 220, sep=20, n=24).as_hex())]),
+        showlegend=False,
+        hoverinfo='text',
+
+        ),
+    ]
+
+    layers=[dict(sourcetype = 'geojson',
+             source =geojson_layer['features'][k]['geometry'],
+             below="water",
+             type = 'fill',
+             name=geojson_layer['features'][k]['properties']['nom'],
+             color = color[define_index_for_color(dic_answers[geojson_layer['features'][k]['properties']['code']][1])],
+             opacity=0.8
+            ) for k in range(len(geojson_layer['features']))]
+
+    layout = go.Layout(
+        title='Pourcentage de Oui - France Metropolitaine : {}%'.format(round(100*dic_answers['all'],1)),
+        autosize=True,
+        hovermode='closest',
+        mapbox=dict(
+            layers=layers,
+            accesstoken=mapbox_access_token,
+            bearing=0,
+            center=dict(
+                lat=46.4,
+                lon=2.3
+            ),
+            pitch=0,
+            zoom=5.2,
+            style='light'
+        )
+    )
+
+    return data, layout
+
+
+def theme_opacity(themes, selected_theme,  n_participants, completion_rates):
     bar_opacity = []
     annotations = []
 
@@ -113,17 +260,19 @@ def theme_opacity(themes, selected_theme, n_questions, n_answers, n_participants
         else:
             bar_opacity.append(transparency["full"])
 
-        annotations.append(dict(x=theme, y=n_questions[i], text=str(n_questions[i]),
+        annotations.append(dict(x=list(themes.values())[i], y=n_participants[i], text="{0:.0f}".format(n_participants[i]/1000) + "k",
                                     xref='x1',
                                     yref='y1',
                                     showarrow=False,
-                                    yshift = 14)
+                                    yshift = 14,
+                                    font=dict(family='Arial', size=26))
                                     )
-        annotations.append(dict(x=theme, y=list(n_answers.values())[i], text="{0:.1f}".format(list(n_answers.values())[i]/1e6) + "M",# annotation point
+        annotations.append(dict(x=list(themes.values())[i], y=completion_rates[i]*100, text="{0:.0f}".format(completion_rates[i]*100)+"%",# annotation point
                                     xref='x2',
                                     yref='y2',
                                     showarrow=False,
-                                    yshift = 14)
+                                    yshift = 14,
+                                    font=dict(family='Arial', size=26))
                                     )
 
 
@@ -146,9 +295,9 @@ app.css.append_css({
 text_color = "black"
 
 colors = {'background_content': 'white',#'#8597AF','white',
-         'background' : '#e8f1f7', #'#45546B',
-         'bar_unselected' : '#2678B2',
-         'bar_selected' : '#FD7F28'
+         'background' : '#e8f1f7',
+         'bar_unselected' : 'rgb(80, 220, 100)',
+         'bar_selected' : 'rgb(46, 139, 87)'
          }
 
 transparency = {"full":1,
@@ -197,10 +346,10 @@ app.layout = html.Div([
             className="tabs",
             #style={"height":"20","verticalAlign":"middle"},
             children=[
-                dcc.Tab(label="Démocration et Citoyenneté", value="Dem"),
-                dcc.Tab(label="Fiscalité et dépenses publiques", value="Fis"),
-                dcc.Tab(label="Transition écologique", value="Eco"),
-                dcc.Tab(label="Organisation de l'état et service publique", value="Org"),
+                dcc.Tab(label="Démocratie et Citoyenneté", value="Dem"),
+                dcc.Tab(label="Fiscalité et Dépenses Publiques", value="Fis"),
+                dcc.Tab(label="Transition Écologique", value="Eco"),
+                dcc.Tab(label="Organisation de l'État et Service Public", value="Org"),
             ],
             value="Dem",
         )
@@ -215,7 +364,7 @@ app.layout = html.Div([
 
         html.Div([
             html.Div([html.P(id="text_theme",
-                            style={'font-size':'32px',
+                            style={'font-size':'200%',
                                     'font-style':'italic',
                                     'line-height':'50px',
                                     'position': 'relative',
@@ -225,7 +374,7 @@ app.layout = html.Div([
                                     })
                             ],
                         style={'width': '40%',
-                                'height': '550px',
+                                'height': 550,
                                 'display': 'inline-block',
                                 'background-color':colors['background_content'],
                                 'border-left': "20px #e8f1f7 solid",
@@ -235,20 +384,27 @@ app.layout = html.Div([
             html.Div([html.H2('Nombre de Participants',
                         style={
                             'display': 'inline-block',
-                            'margin-left':'200px',
+                            'width':'50%',
+                            'text-align':'center',
                             'color': "black",
                             'border-top': "20px white solid"}),
 
-                    html.H2('Pourcentage de',
+                    html.H2("  Niveau d'implication des participants",
                         style={
                             'float': 'right',
-                            'margin-right':'200px',
+                            'width':'50%',
+                            'text-align':'center',
                             'color': "black",
                             'border-top': "20px white solid"}),
 
-                    dcc.Graph(id='global_stats')],
+                    dcc.Graph(id='global_stats',
+                              style={
+                                  'height': 525,
+                                  "display": "block",
+                                  "margin-top": -75})],
+
                         style={'width': '60%',
-                                'height': '550px',
+                                'height': 550,
                                 'float':'right',
                                 'border-left': "20px #e8f1f7 solid",
                                 'border-right': "20px #e8f1f7 solid",
@@ -269,10 +425,13 @@ app.layout = html.Div([
                         ),
                 dcc.Dropdown(id='question_choice' , value='Q1'),
 
-                dcc.Graph(id='figure_questions')],
+                dcc.Graph(id='figure_questions',
+                            style={'border-left': "100px white solid",
+                                    'height': '80%',
+                                    'width': '80%'})],
 
                 style={'width': '59%',
-                       'height': '800px',
+                       'height': 1300,
                        'display': 'inline-block',
                        'border-left': "20px #e8f1f7 solid",
                        'border-top': "20px #e8f1f7 solid",}),
@@ -285,10 +444,15 @@ app.layout = html.Div([
                         'color': "black",
                         'border-top': "10px white solid"}),
 
-                dcc.Graph(id='answer_rate')],
+                dcc.Graph(id='answer_rate',
+                          style={
+                            'height': 1050,
+                            "display": "block",
+                            "margin-left": "auto",
+                            "margin-right": "auto"})],
 
                 style={'width': '41%',
-                        'height': '800px',
+                        'height': 1300,
                         'float': 'right',
                         'display': 'inline-block',
                         'border-left': "20px #e8f1f7 solid",
@@ -317,7 +481,8 @@ def update_page(selected_theme, selected_question):
     data = get_data(selected_theme)
     text_theme = get_theme_text(selected_theme)
     questions_num, answer_rate, question_type, questions, questions_formated = get_questions(data)
-    n_questions, n_participants, theme_answer_rate, avg_answer_per_participant = get_stats(data_dict, overall_stats)
+    n_participants, completion_rates, n_questions = get_stats(data_dict, n_answers)
+
 
     if selected_question==None:
         selected_question=questions_num[0]
@@ -327,13 +492,8 @@ def update_page(selected_theme, selected_question):
 
     # Defining specific color for selected question, and creating annotation list
     bar_colors, annotations_1 = color_question(questions_num, selected_question, answer_rate)
-    bar_opacity, annotations_2 = theme_opacity(themes, selected_theme, n_questions, n_answers, n_participants, avg_answer_per_participant)
+    bar_opacity, annotations_2 = theme_opacity(themes, selected_theme,  n_participants, completion_rates)
 
-    # Getting global statistics for selected theme
-    stat_names = overall_stats
-    values = []
-    for stat in stat_names:
-        values.append(data[stat])
 
     # Extract the more frequent words of the theme open questions
     top_words_list = get_open_questions_words(data)
@@ -342,8 +502,10 @@ def update_page(selected_theme, selected_question):
     if question_type[questions_num.index(selected_question)]=="binary":
         # If binary question : figure=heatmap
         # TO DO cf Théo
-        question_data = [go.Bar(x=["MAP"], y=[0])]
-        question_layout = None
+        quest = questions_num.index(selected_question)
+        data_binary, layout_binary = get_closed_question_params(data, selected_question)
+        question_data = data_binary
+        question_layout = layout_binary
 
 
     else:
@@ -381,41 +543,43 @@ def update_page(selected_theme, selected_question):
                     showticklabels=False),
             plot_bgcolor = colors["background_content"],
             paper_bgcolor = colors["background_content"],
+            font=dict(family='Arial', size=26),
             annotations = annotations_1)
             }
 
     y_axis_dict = dict(
-            zeroline=False,
             showline=False,
-            ticks='',
+            autorange=True,
+            showgrid=False,
+            zeroline=False,
             showticklabels=False)
 
     figure_global_stats = {
         'data':[go.Bar(
-                    y=n_questions,
-                    x=list(data_dict.keys()),
+                    y=n_participants,
+                    x=list(themes.values()),
                     xaxis='x1',
                     yaxis='y1',
                     marker = dict(opacity=bar_opacity),
                     hoverinfo = "none"),
                 go.Bar(
-                    y= list(n_answers.values()),
-                    x=list(data_dict.keys()),
-                    xaxis='x2',
-                    yaxis='y2',
+                    y = [rate*100 for rate in completion_rates],
+                    x =list(themes.values()),
+                    xaxis ='x2',
+                    yaxis ='y2',
                     marker = dict(opacity=bar_opacity),
                     hoverinfo = "none")],
         'layout': go.Layout(
-            showlegend=False,
-            grid= dict(rows= 1, columns= 2),
-            xaxis1=global_stats_axis_font[0],
-            xaxis2=global_stats_axis_font[1],
+            showlegend = False,
+            grid = dict(rows= 1, columns= 2),
+            xaxis1 = global_stats_axis_font[0],
+            xaxis2 = global_stats_axis_font[1],
             yaxis1 = y_axis_dict,
             yaxis2 = y_axis_dict,
-            annotations=annotations_2,
-            titlefont= chart_title_font,
-            plot_bgcolor = colors["background_content"],
-            paper_bgcolor = colors["background_content"])
+            annotations = annotations_2,
+            font=dict(family='Arial', size=26),
+            plot_bgcolor = 'rgba(0,0,0,0)',
+            paper_bgcolor = 'rgba(0,0,0,0)')
                     }
 
 
